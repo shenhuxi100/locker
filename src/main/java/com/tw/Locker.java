99cf4b18b63c000d9ef0795299820765dd8bc0fe
Jing Liang <jing.liang@scuvis.org>
2020-06-12 17:15:38 +0800

    refactor: remove test comment and use capacity as ticket number

--- src/main/java/com/tw/Locker.java
+++ src/main/java/com/tw/Locker.java
@@ -1,33 +1,32 @@
 package com.tw;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Locker {
     private int surplusCapacity;
-    private Map<String, String> ticketLocker = new HashMap<>();
+    private Map<Integer, String> ticketLocker = new HashMap<>();
 
     public Locker(int surplusCapacity) {
         this.surplusCapacity = surplusCapacity;
     }
 
-    public String saveBag() {
-        if(surplusCapacity <= 0) {
+    public Integer saveBag() {
+        if (surplusCapacity <= 0) {
             System.out.println("出票失败");
             return null;
         }
 
+        Integer ticketNumber = surplusCapacity;
         surplusCapacity--;
-        String ticketNumber = LocalDateTime.now().toString();
         ticketLocker.put(ticketNumber, "1");
         System.out.println("出票成功");
         return ticketNumber;
     }
 
-    public boolean takeBag(String ticket) {
+    public boolean takeBag(Integer ticket) {
         String locker = ticketLocker.get(ticket);
-        if (locker != null){
+        if (locker != null) {
             ticketLocker.remove(ticket);
             System.out.println("取包成功");
             surplusCapacity++;
--- src/test/java/com/tw/LockerTest.java
+++ src/test/java/com/tw/LockerTest.java
@@ -3,8 +3,6 @@ package com.tw;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
@@ -14,29 +12,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class LockerTest {
     @Test
     void should_prompt_failure_and_no_ticket_pop_when_save_bag_given_locker_is_full() {
-        // given
         int surplusCapacity = 0;
         Locker locker = new Locker(surplusCapacity);
 
-        //when
-        String result = locker.saveBag();
+        Integer result = locker.saveBag();
 
-        // then
         assertNull(result);
         assertThat(locker.getSurplusCapacity(), is(0));
     }
 
     @Test
     void should_save_bag_successfully_and_pop_ticket_when_save_bag_given_locker_is_not_full() {
-        // given
         int surplusCapacity = 19;
         Locker locker = new Locker(surplusCapacity);
-        String ticketNumber = LocalDateTime.now().toString();
+        Integer ticketNumber = surplusCapacity;
 
-        // when
-        String result = locker.saveBag();
+        Integer result = locker.saveBag();
 
-        // then
         assertThat(result, is(ticketNumber));
         assertThat(locker.getSurplusCapacity(), is(18));
 
@@ -44,15 +36,12 @@ class LockerTest {
 
     @Test
     void should_prompt_failure_when_take_bag_given_ticket_is_invalid() {
-        // given
         int surplusCapacity = 2;
-        String ticket = "Invalid ticket";
+        Integer ticket = 3;
         Locker locker = new Locker(surplusCapacity);
 
-        // when
         boolean result = locker.takeBag(ticket);
 
-        // then
         assertFalse(result);
         assertThat(locker.getSurplusCapacity(), is(2));
 
@@ -60,30 +49,24 @@ class LockerTest {
 
     @Test
     void should_prompt_failure_when_take_bag_given_ticket_is_duplicated() {
-        // given
         int surplusCapacity = 10;
         Locker locker = new Locker(surplusCapacity);
-        String ticket = locker.saveBag();
+        Integer ticket = locker.saveBag();
 
-        // when
         boolean result = locker.takeBag(ticket);
 
-        // then
         assertTrue(result);
         assertFalse(locker.takeBag(ticket));
     }
 
     @Test
     void should_take_bag_successfully_when_take_bag_given_ticket_is_valid() {
-        // given
         int surplusCapacity = 10;
         Locker locker = new Locker(surplusCapacity);
-        String ticket = locker.saveBag();
+        Integer ticket = locker.saveBag();
 
-        // when
         boolean result = locker.takeBag(ticket);
 
-        // then
         assertTrue(result);
         assertThat(locker.getSurplusCapacity(), is(10));
     }
