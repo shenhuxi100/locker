9caf60939d19451c329d4513144bfa4cd3a0f5be
yongzhiliu <393073080@qq.com>
2020-06-14 21:41:29 +0800

    Given: 一张有效票， When: 取包 Then: 机器人返回一个包

--- src/test/java/com/tw/locker/model/RobotTest.java
+++ src/test/java/com/tw/locker/model/RobotTest.java
@@ -9,10 +9,6 @@ import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-/**
- * Given: 一张无效票， When: 取包 Then: 提示无效票据
- * Given: 一张有效票， When: 取包 Then: 机器人返回一个包
- */
 public class RobotTest {
     @Test
     void should_return_ticket_and_store_bag_in_1st_locker_when_save_bag_given_robot_have_2_under_filled_lockers() {
@@ -51,5 +47,15 @@ public class RobotTest {
         Robot robot = new Robot(Arrays.asList(new Locker(1), new Locker(1)));
         Ticket invalidTicket = new Ticket();
         assertThrows(InvalidTicketException.class, () -> robot.takeBag(invalidTicket));
+    }
+
+    @Test
+    void should_return_bag_when_take_bag_given_valid_ticket() {
+        Robot robot = new Robot(Arrays.asList(new Locker(1), new Locker(1)));
+        Bag bag = new Bag();
+        Ticket validTicket = robot.saveBag(bag);
+
+        Bag returnBag = robot.takeBag(validTicket);
+        assertEquals(bag, returnBag);
     }
 }
