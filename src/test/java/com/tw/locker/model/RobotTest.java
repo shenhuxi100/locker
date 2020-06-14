8ccdc7e7fe8c456d71cb1dbb47ad5ae17b04b7f9
yongzhiliu <393073080@qq.com>
2020-06-14 21:30:06 +0800

    一个机器人有2的柜子，两个柜子都满， When: 存包 Then: 存包失败，提示柜子已满

--- src/test/java/com/tw/locker/model/RobotTest.java
+++ src/test/java/com/tw/locker/model/RobotTest.java
@@ -1,13 +1,14 @@
 package com.tw.locker.model;
 
+import com.tw.locker.exception.NoCapacityException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
- * Given: 一个机器人有2的柜子，第一个柜子已满，第二个柜子有空， When: 存包 Then: 存第二个柜子，返回一张票
  * Given: 一个机器人有2的柜子，两个柜子都满， When: 存包 Then: 存包失败，提示柜子已满
  * Given: 一张无效票， When: 取包 Then: 提示无效票据
  * Given: 一张有效票， When: 取包 Then: 机器人返回一个包
@@ -19,7 +20,7 @@ public class RobotTest {
         Bag bag = new Bag();
         Ticket ticket = robot.saveBag(bag);
 
-        // assertNotNull(ticket); //looks duplicated here, so comment here
+        // assertNotNull(ticket); //looks duplicated test here, so comment here
         assertEquals(robot.getLockers().get(0).takeBag(ticket), bag);
     }
 
@@ -31,5 +32,17 @@ public class RobotTest {
         Ticket secondTicket = robot.saveBag(bag);
 
         assertEquals(robot.getLockers().get(1).takeBag(secondTicket), bag);
+    }
+
+    @Test
+    void should_throw_NoCapacityException_when_save_bag_given_robot_have_two_full_filled_lockers() {
+        Robot robot = new Robot(Arrays.asList(new Locker(1), new Locker(1)));
+        Bag bag = new Bag();
+        robot.saveBag(bag);
+        bag = new Bag();
+        robot.saveBag(bag);
+
+        Bag thirdBag = new Bag();
+        assertThrows(NoCapacityException.class, () -> robot.saveBag(thirdBag));
     }
 }
