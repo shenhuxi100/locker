28a44fadde7978c8b7a165ac11b25dfac39e819d
Jing Liang <jing.liang@scuvis.org>
2020-06-29 00:01:34 +0800

    Given director管理两个manager，manager1管理两个robot，不管理locker；manager2不管理robot，只管理locker

--- src/test/java/com/tw/locker/director/LockerRobotDirectorTest.java
+++ src/test/java/com/tw/locker/director/LockerRobotDirectorTest.java
@@ -12,10 +12,6 @@ import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
-/*
- * Given director管理一个manager，manager既管理locker，也管理robot
- * Given director管理两个manager，manager1管理两个robot，不管理locker；manager2不管理robot，只管理locker
- */
 class LockerRobotDirectorTest {
     @Test
     void should_return_report_with_MRL_when_LockerRobotDirector_view_report_given_1_manager_which_manage_1_robot_with_1_locker() {
@@ -112,6 +108,34 @@ class LockerRobotDirectorTest {
                 "\t\tL 0 1\n" +
                 "\tR 1 2\n" +
                 "\t\tL 1 2\n";
+        assertEquals(expectReport, report);
+    }
+
+    @Test
+    void should_return_report_with_MRLRLML_when_LockerRobotDirector_view_report_given_1st_manager_which_manage_2_robots_each_with_1_locker_2nd_manager_only_manage_1_locker() {
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(singletonList(new Locker(1)));
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(singletonList(new Locker(2)));
+        LockerRobotManager lockerRobotManager1 = new LockerRobotManager(emptyList(), asList(smartLockerRobot, primaryLockerRobot));
+        Locker locker = new Locker(2);
+        LockerRobotManager lockerRobotManager2 = new LockerRobotManager(singletonList(locker), emptyList());
+        LockerRobotDirector lockerRobotDirector = new LockerRobotDirector(asList(lockerRobotManager1, lockerRobotManager2));
+
+        Bag bag = new Bag();
+        lockerRobotManager1.saveBag(bag);
+        bag = new Bag();
+        lockerRobotManager1.saveBag(bag);
+        bag = new Bag();
+        lockerRobotManager2.saveBag(bag);
+
+        String report = lockerRobotDirector.viewReport();
+
+        String expectReport = "M 1 3\n" +
+                "\tR 0 1\n" +
+                "\t\tL 0 1\n" +
+                "\tR 1 2\n" +
+                "\t\tL 1 2\n" +
+                "M 1 2\n" +
+                "\tL 1 2\n";
         assertEquals(expectReport, report);
     }
 }
